5cf1db906c44aea06cec7e368ed2ed306fac494f
Étienne Ameye <etienne.ameye@videotron.ca>
2022-02-01 12:52:24 -0500

    First implementation of ClassCounter
    The ClassCounter can now count non-empty line. Will add comment counting later.

--- src/ClassCounter.java
+++ src/ClassCounter.java
@@ -1,20 +1,79 @@
+import io.Reader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class ClassCounter
 {
     private String filePath;
+
+    private int loc = 0;
+    private int cloc = 1;
 
     public ClassCounter(String filePath)
     {
         this.filePath = filePath;
     }
 
+    /**
+     * Read the content of the file and convert it to a {@link Reader}.
+     *
+     * @return A reader of the content of the file
+     * @throws IOException If an error occur during the reading
+     */
+    private Reader readFile() throws IOException
+    {
+        File file = new File(filePath);     //The file to read
+
+        //Open the file
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file)))
+        {
+            StringBuilder text = new StringBuilder();
+
+            //Read the content of the file
+            int c = bufferedReader.read();
+            while(c != -1)
+            {
+                text.append((char) c);
+                c = bufferedReader.read();
+            }
+
+            //Create a reader with the content
+            return new Reader(text.toString());
+        }
+    }
+
+    public void read() throws IOException
+    {
+        Reader reader = readFile();
+
+        //read and count
+        List<String> line = reader.readLine();
+
+        //Count the lines
+        while(line != null)
+        {
+            //Only count non-empty line for now
+            if(!line.isEmpty())
+            {
+                loc++;
+            }
+
+            line = reader.readLine();       //Move to the next line
+        }
+    }
+
     public int getLOC()
     {
-        return 10;
+        return loc;
     }
 
     public int getCLOC()
     {
-        return 5;
+        return cloc;
     }
 
     public int getDC()
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         //main function
+
+        String filePath = "test";
+
+        ClassCounter counter = new ClassCounter(filePath);
+        counter.read();
+
+        System.out.println(counter.getLOC());
     }
 }
